c36c28b30a1a2254edd19c0d1a9c354d392822ca
Kay Kang <kkang1119@gmail.com>
2022-05-06 17:59:33 -0400

    finished js exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+  if (num < 0) {
+    return "ERROR";
+  } else {
+    return `${string.repeat(num)}`;
+  }
+};
 
+const repeatString2 = function (word, times) {
+  if (times < 0) return "ERROR";
+  let string = "";
+  for (let i = 0; i < times; i++) {
+    string += word;
+  }
+  return string;
 };
 
 // Do not edit below this line
