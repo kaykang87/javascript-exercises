aeeb2f6a4a4922cae0af97e19e5f8674186e1aa3
Kay Kang <kkang1119@gmail.com>
2022-05-07 19:38:28 -0400

    modified leapyear

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
+const leapYears = function (leapYear) {
+    if (leapYear % 4 === 0) {
+        if (leapYear % 4 === 0 && leapYear % 400 === 0) return true;
+        if (leapYear % 4 === 0 && leapYear % 100 === 0) return false;
+        return true;
+    } else return false;
 
+    // return year % 4 === 0 && ( year % 100 !== 0 || year % 400 === 0);
 };
 
 // Do not edit below this line
