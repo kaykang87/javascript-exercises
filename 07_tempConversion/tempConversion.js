aa360656d08846021d1bddd24d3e6f2d608498e9
Kay Kang <kkang1119@gmail.com>
2022-05-07 19:37:33 -0400

    finished js exercise upto tempconversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function (far) {
+  let celsius = (far - 32) * (5 / 9);
+  return Number(celsius.toFixed(1));
 };
 
-const ctof = function() {
-
+const ctof = function (cel) {
+  let fahrenheit = cel * (9 / 5) + 32;
+  return Number(fahrenheit.toFixed(1));
 };
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
