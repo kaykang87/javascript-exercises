4bdb7f770df70d7dfe2243ba7b9d86ba78d64fd9
Kay Kang <kkang1119@gmail.com>
2022-05-07 14:18:41 -0400

    finished reversing and removing array exercises

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, ...remove) {
+  filtered = array.filter((e) => !remove.includes(e));
+  return filtered;
 };
+
+removeFromArray([1, 2, 3, 4, 5, 6, 7, 8], 3, 5);
 
 // Do not edit below this line
 module.exports = removeFromArray;
