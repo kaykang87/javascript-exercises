1907333476c8ea27018b4d60d30eef47bcd6568a
Kay Kang <kkang1119@gmail.com>
2022-05-15 19:06:53 -0400

    finished js exercises using reduce function

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = (array) => {
+  return array.reduce((previousAge, currentAge) => {
+    const previousPerson = getAge(
+      previousAge.yearOfDeath,
+      previousAge.yearOfBirth
+    );
+    const currentPerson = getAge(
+      currentAge.yearOfDeath,
+      currentAge.yearOfBirth
+    );
+    return previousPerson > currentPerson ? previousAge : currentAge;
+  });
+};
 
+const getAge = function (death, birth) {
+  if (!death) {
+    death = new Date().getFullYear();
+  }
+  return death - birth;
 };
 
 // Do not edit below this line
