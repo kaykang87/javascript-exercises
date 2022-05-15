900330d19e485cc325d09a9bb2b6acd68eb5fe2d
Kay Kang <kkang1119@gmail.com>
2022-05-15 16:48:51 -0400

    finished palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (pal) {
+  // check if a word is spelled the same way backward
+  let forward = pal.toLowerCase().replace(/[^a-zA-z]+/g, "");
+  let backward = [...pal]
+    .reverse()
+    .join("")
+    .toLowerCase()
+    .replace(/[^a-zA-z]+/g, "");
+  console.log(forward, backward);
+  return forward === backward;
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
